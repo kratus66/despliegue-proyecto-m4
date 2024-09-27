9aca5708c8592e1ec49fd1583269b6578df75395
kratus66 <diegoherrera1685@hotmail.com>
2024-09-27 09:19:01 -0500

    mi primer commit

---
+++ front/next.config.mjs
@@ -0,0 +1,41 @@
+// @type {import('next').NextConfig}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'www.apple.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: '**.ngrok-free.app',
+        port: '', // Puedes dejarlo vacío si no tienes un puerto específico
+        pathname: '/images/**', // Ajusta el pathname si es necesario
+      },
+      {
+        protocol: 'https',
+        hostname: 'w7.pngwing.com',
+        pathname: '/**',
+      },
+      {
+        protocol: 'http',
+        hostname: 'localhost',
+        port: '3001',
+        pathname: '/images/**',
+      },
+
+    ],
+  },
+  // Configuración de variables de entorno
+  env: {
+    API_URL: process.env.API_URL || 'http://localhost:3001',  // Define una URL predeterminada si no está configurada
+  },
+};
+
+export default nextConfig;
+
+
+
+
+
